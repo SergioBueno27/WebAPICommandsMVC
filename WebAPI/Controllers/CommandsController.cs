859ffd3769a70d04314a92bf02eecf0afdfc58c0
Sergio Bueno Medina <sergan007@gmail.com>
2020-07-25 19:30:14 +0200

    Small typo in this file

--- WebAPI/Controllers/CommandsController.cs
+++ WebAPI/Controllers/CommandsController.cs
@@ -27,7 +27,7 @@ namespace Commander.Controllers
         //GET api/commands
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands() {
-            var commandItems = _repository.GetAppCommands();
+            var commandItems = _repository.GetAllCommands();
             return Ok(commandItems);
         }
 
