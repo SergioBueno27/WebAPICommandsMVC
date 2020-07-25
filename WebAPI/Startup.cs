1d4d1b5ccc883a0574eda401ae7de15bbd37497c
Sergio Bueno Medina <sergan007@gmail.com>
2020-07-25 18:42:14 +0200

    Added Nuget packages and added Context for Command model through an SQL server database

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             //Added service to point to the implementation to our repository
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
