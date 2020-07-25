b366c669ad6623913f8ae35e40fc3859e2b1d9f7
Sergio Bueno Medina <sergan007@gmail.com>
2020-07-25 20:06:40 +0200

    Change use to CommandReadDto

--- WebAPI/Controllers/CommandsController.cs
+++ WebAPI/Controllers/CommandsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Commander.Data;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Data;
+using WebAPI.Dtos;
 using WebAPI.Models;
 
 namespace Commander.Controllers
@@ -17,27 +19,31 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+
+        public readonly IMapper _mapper;
+
         //Since we added the service on the startup using MockCommanderRepo every time
         //we use ICommanderRepo, using dependency injection
         //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
-        public CommandsController(ICommanderRepo repository)
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
         //GET api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands() {
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands() {
             var commandItems = _repository.GetAllCommands();
             return Ok(commandItems);
         }
 
     //GET api/commands/5
     [HttpGet("{id}")]
-        public ActionResult<Command> GetCommandById(int id) { 
+        public ActionResult<CommandReadDto> GetCommandById(int id) { 
             var commandItem = _repository.GetCommandById(id);
             if (commandItem != null)
             {
-                return Ok(commandItem);
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
             }
             else 
             {
